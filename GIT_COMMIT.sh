cf41d56037f4167ef95e2e327fc276be21f84af1
mitko <dimashenkov@abv.bg>
2018-01-12 14:37:17 +0000

    eeeee

--- GIT_COMMIT.sh
+++ GIT_COMMIT.sh
@@ -1,4 +1,4 @@
  git add .
   git commit 
-     git push -u origin master
-git push dimashenkov:Bullet74!@https://github.com/dimashenkov/chef2/file.git
+#     git push -u origin master
+#git push dimashenkov:Bullet74!@https://github.com/dimashenkov/chef2/file.git
