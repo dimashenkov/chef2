72eab92009d705cce4ac5abaf5b33f9c218d0b91
mitko <dimashenkov@abv.bg>
2018-01-17 10:02:40 +0000

    db coock

--- cookbooks/postgres/recipes/default.rb
+++ cookbooks/postgres/recipes/default.rb
@@ -5,8 +5,8 @@
 # Copyright:: 2018, The Authors, All Rights Reserved.
 
 
-package 'postgreql-server' do
-	notifies :run, 'execute[postgresql-init]'
+package 'postgresql-server' do
+	notifies :run, 'execute[postgresql-init]' , :immediately
 end
 execute 'postgresql-init' do
 	command 'postgresql-setup initdb'
