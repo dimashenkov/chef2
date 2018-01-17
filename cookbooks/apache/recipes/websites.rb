f5cd317adacd921896983e8eab4b096f54e2a435
mitko <dimashenkov@abv.bg>
2018-01-17 14:08:25 +0000

    search  function

--- cookbooks/apache/recipes/websites.rb
+++ cookbooks/apache/recipes/websites.rb
@@ -1,5 +1,11 @@
 file 'default ww' do
 	path '/var/www/html/index.html'
-	content 'hellow mitko33133'
+	content 'hellow mitko2223133'
+end
+webnodes = search('node', 'role:web')
+
+
+webnodes.each do |node|
+	puts node
 end
 
